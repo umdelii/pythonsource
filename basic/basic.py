b91661a447623f7f4fd0974bbb6f178bc2008b32
umdelii <sang9205@gmail.com>
2026-02-05 18:01:52 +0900

    python 기초개념

--- basic/basic.py
+++ basic/basic.py
@@ -1,6 +1,18 @@
 # 주석
-print('python programing')
+print("python programing")
 
 # 키보드에서 입력 받기
-msg = input('input num : ')
-print(msg)
+# msg = input('input num : ')
+# print(msg)
+
+str1 = "Life is too short, You need Python"
+str1 = """Life is too short, You need Python"""
+str1 = """Life is too short, You need Python"""
+
+print(str1)
+
+str1 = """\
+Life is too short, You need Python
+"""
+
+print(str1)
